273f0275c4a6dedf0795b7a93dc21d49e30d5b17
Romanhey <romanmankevich09@gmail.com>
2024-12-02 18:03:22 +0300

    main page(media) and profile
    (cherry picked from commit 2c0984f69062a4004a3b493371e5a2f3f4e594b4)

--- WebDev/Client/romicfood/src/Components/Header/Header.jsx
+++ WebDev/Client/romicfood/src/Components/Header/Header.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { FiShoppingCart, FiTruck } from "react-icons/fi"; // Добавляем иконку для логотипа
+import { TbTruckDelivery } from "react-icons/tb";
 
-function Header({isAuth}) {
+function Header({ isAuth }) {
+    const [menuOpen, setMenuOpen] = useState(false);
 
-    console.log(isAuth);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
         <header>
             <nav>
                 <div className="nav_logo">
-                    <img src={`${process.env.PUBLIC_URL}/img/Group 5.svg`} alt="logo"/>
+                    <TbTruckDelivery /> {/* Используем иконку FiTruck для логотипа */}
                 </div>
+                <div className="navigation">
                 <div className="nav_search">
                     <input type="text" placeholder="Search"/>
                     <img src={`${process.env.PUBLIC_URL}/img/icons-reach.svg`} alt="search icon"/>
                 </div>
-                <div className="nav_buttons">
-                    <a href="">Сообщество</a>
-                    <a href="">Ресурсы</a>
-                    <a href="">Контакты</a>
-                    <a href="" id="cart_button">
-                        <img src={`${process.env.PUBLIC_URL}/img/icons-cart.png`} alt="asd"/>
+                    <div className={`nav_buttons ${menuOpen ? 'open' : ''}`}>
+                        <a href="">Сообщество</a>
+                        <a href="">Ресурсы</a>
+                        <a href="">Контакты</a>
+                    </div>
+                    <a href="" id="cart_button" className="cart-icon">
+                        <FiShoppingCart/>
                     </a>
-                    <div className="nav_profile">
+                    <div className={`nav_buttons 0{menuOpen ? 'open' : ''}`}>
                         <div className="nav_profile">
                             <NavLink
                                 to="/profile"
@@ -40,14 +47,16 @@ function Header({isAuth}) {
                                 Войти
                             </NavLink>
                         </div>
-
-
                     </div>
                 </div>
+
+                <div className="burger-menu" onClick={toggleMenu}>
+                    <div className="burger-icon"></div>
+                    <div className="burger-icon"></div>
+                    <div className="burger-icon"></div>
+                </div>
             </nav>
-            <div
-                className="header_content"
-            >
+            <div className="header_content">
                 <div className="header_content_text">
                     <h1>Абсолютно бесплатная еда</h1>
                     <h3>бесплатная еда каждый час</h3>
