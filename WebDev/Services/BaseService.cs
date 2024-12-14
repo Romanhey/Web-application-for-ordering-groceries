b0db02c3355fb096c20f65b8e8cb0ae1f9dc9c7d
Romanhey <romanmankevich09@gmail.com>
2024-12-14 21:35:45 +0300

    add abstract class baseService

---
+++ WebDev/Services/BaseService.cs
@@ -0,0 +1,13 @@
+﻿using WebDev.Models;
+
+namespace WebDev.Services
+{
+    public abstract class BaseService
+    {
+        public ApplicationDBContext _context;
+        public BaseService(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+    }
+}
