42f750421fba0b2e19a1ea4af954d2c362d8e9bc
Romanhey <romanmankevich09@gmail.com>
2024-12-18 21:56:54 +0300

    g

--- Client/romicfood/src/Components/ProductMenu/ProductMenu.jsx
+++ Client/romicfood/src/Components/ProductMenu/ProductMenu.jsx
@@ -33,10 +33,10 @@ function ProductMenu({setProductMenu,product,isAdmin}) {
                     {product?.productName}
                 </div>
                 <div className="product-menu__price">
-                    {product?.price} BYN
+                    Цена: {product?.price} BYN
                 </div>
                 <div className="product-menu__category">
-                    {product?.category?.categoryName}
+                    Категория: {product?.category?.categoryName}
                 </div>
                 <div className="product-menu__description">
                     Описание: {product?.productDescription}
